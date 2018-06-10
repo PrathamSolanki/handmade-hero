6635cde8f1c8c82eca55f1a4894bde21c6529798
Pratham Solanki <pratham.c.solanki@gmail.com>
2018-06-10 12:24:08 +0530

    end of stream 1: Day 001: Setting Up the Windows Build

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(0, "Game Development journey begins.", "Welcome!", MB_OK|MB_ICONINFORMATION);
+
+	return(0);
+}  
